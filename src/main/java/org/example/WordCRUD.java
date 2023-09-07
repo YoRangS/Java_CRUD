0adf44a093c69a452867850e141ee9956635d8de
JungEunchong <21900673@handong.ac.kr>
2023-09-07 16:10:46 +0900

    Exception Handling

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -97,31 +97,39 @@ public class WordCRUD implements ICRUD{
 		System.out.print("=> 수정할 단어 검색 : ");
 		String keyword = s.next();
 		ArrayList<Integer> idlist = this.listAll(keyword);
-		System.out.print("=> 수정할 번호 선택 : ");
-		int id = s.nextInt();
-		s.nextLine();
-		System.out.print("=> 뜻 입력 : ");
-		String meaning = s.next();
-		Word word = list.get(idlist.get(id-1));
-		word.setMeaning(meaning);
-		System.out.println("단어가 수정되었습니다.");
+		if(!idlist.isEmpty()) {
+			System.out.print("=> 수정할 번호 선택 : ");
+			int id = s.nextInt();
+			s.nextLine();
+			System.out.print("=> 뜻 입력 : ");
+			String meaning = s.next();
+			Word word = list.get(idlist.get(id-1));
+			word.setMeaning(meaning);
+			System.out.println("단어가 수정되었습니다.");
+		} else {
+			System.out.println("수정할 단어가 없습니다.");
+		}
 	}
 
 	public void deleteItem() {
 		System.out.print("=> 삭제할 단어 검색 : ");
 		String keyword = s.next();
 		ArrayList<Integer> idlist = this.listAll(keyword);
-		System.out.print("=> 삭제할 번호 선택 : ");
-		int id = s.nextInt();
-		s.nextLine();
+		if(!idlist.isEmpty()) {
+			System.out.print("=> 삭제할 번호 선택 : ");
+			int id = s.nextInt();
+			s.nextLine();
 
-		System.out.print("=> 정말로 삭제하실래요?(Y/n) ");
-		String ans = s.next();
-		if(ans.equalsIgnoreCase("y")) {
-			list.remove((int)idlist.get(id-1));
-			System.out.println("단어가 삭제되었습니다. ");
+			System.out.print("=> 정말로 삭제하실래요?(Y/n) ");
+			String ans = s.next();
+			if (ans.equalsIgnoreCase("y")) {
+				list.remove((int) idlist.get(id - 1));
+				System.out.println("단어가 삭제되었습니다. ");
+			} else {
+				System.out.println("취소되었습니다. ");
+			}
 		} else {
-			System.out.println("취소되었습니다. ");
+			System.out.println("삭제할 단어가 없습니다.");
 		}
 	}
 
